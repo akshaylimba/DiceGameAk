631fa6d11a22e2c7e4f15cf69defe03bbab19a2e
akshaylimba <112004724+akshaylimba@users.noreply.github.com>
2022-11-02 04:08:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function dice()
     var s2;
     a1 = Math.floor(Math.random()*6)+1;
     a2 = 'images/dice'+a1 +'.png';
-    s2 = 'images/s'+a1 +'.png';
+    s2 = 'images/s'+a1 +'.PNG';
     document.getElementById("images1").src =s2;
     document.getElementById("imagedice1").src =a2;
 
