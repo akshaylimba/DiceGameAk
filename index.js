0cd5308cbd499dbeaf07a5814a4c5ff44cc0cef0
akshaylimba <112004724+akshaylimba@users.noreply.github.com>
2022-11-02 04:19:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ function dice()
     var s2;
     a1 = Math.floor(Math.random()*6)+1;
     a2 = 'images/dice'+a1 +'.png';
-    s2 = 'images/s'+a1 +'.jpg';
+    s2 = 'images/s'+a1 +'.PNG';
     document.getElementById("images1").src =s2;
     document.getElementById("imagedice1").src =a2;
 
     a1 = Math.floor(Math.random()*6)+1;
     a2 = 'images/dice'+a1 +'.png';
-    s2 = 'images/s'+a1 +'.jpg';
+    s2 = 'images/s'+a1 +'.PNG';
     document.getElementById("imagedice2").src =a2;
     document.getElementById("images2").src =s2;
 }
